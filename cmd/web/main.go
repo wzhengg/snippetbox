3574255db1acfda3c313d2af10af781718d056d2
William Zheng <williamzheng2001@gmail.com>
2024-03-27 15:18:31 -0400

    Organize project

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -19,8 +19,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
