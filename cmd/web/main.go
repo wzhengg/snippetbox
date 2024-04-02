1a168fdd5084662bbee40be90bc9fbd808e2234f
William Zheng <williamzheng2001@gmail.com>
2024-04-02 10:44:09 -0400

    Add snippet model to application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,12 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/wzhengg/snippetbox/internal/models"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -29,7 +31,10 @@ func main() {
 
 	defer db.Close()
 
-	app := &application{logger}
+	app := &application{
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
+	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
