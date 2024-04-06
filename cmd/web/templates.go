50c774a4b6984d6da8c3454168875bfe376de0a9
William Zheng <williamzheng2001@gmail.com>
2024-04-06 12:16:14 -0400

    Create in-memory cache of parsed templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,6 +1,39 @@
 package main
 
-import "github.com/wzhengg/snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/wzhengg/snippetbox/internal/models"
+)
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
 
 type templateData struct {
 	Snippet  models.Snippet
