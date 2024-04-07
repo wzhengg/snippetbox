bc7463dc87ed930d7705f27b523905d1da3ff822
William Zheng <williamzheng2001@gmail.com>
2024-04-07 09:14:44 -0400

    Add helper to create templateData with year

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -42,4 +43,10 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 	w.WriteHeader(status)
 
 	buf.WriteTo(w)
+}
+
+func (app *application) newTemplateData(r *http.Request) templateData {
+	return templateData{
+		CurrentYear: time.Now().Year(),
+	}
 }
