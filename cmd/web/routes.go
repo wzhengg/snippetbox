9dd787e7a9a1afd0b92a1a596ac156a05937903a
William Zheng <williamzheng2001@gmail.com>
2024-04-30 20:47:07 -0400

    Add ping route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,8 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
+	mux.HandleFunc("GET /ping", ping)
+
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
